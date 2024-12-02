6d6ddfa6abdfe8c0a4f4eac15ccd6232e46e3f4e
patrick-ouano <patrick.ouano@ufl.edu>
2024-12-02 15:44:25 -0500

    -added mole

--- whackamole.py
+++ whackamole.py
@@ -14,7 +14,8 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+            screen.fill(213, 247, 242)
+            screen.blit(mole_image, mole_image.get_rect(topleft=(x, y)))
             pygame.display.flip()
             clock.tick(60)
     finally:
